5cab2cb6fde0ae6609146e4fc1f9b8ad9a7d35e6
Taha <moriswala3880@gmail.com>
2022-06-28 23:30:48 -0700

    change dashboard message

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,15 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                    <div class="mt-8 text-2xl">
+                        Users
+                    </div>
+
+                    <div class="mt-6">
+                        <h3>{{ __('Hello').' '.auth()->user()->name.' & your balance is '.auth()->user()->balance }}</h3>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
